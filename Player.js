83a60515bc997daf3dbe88488d84cd5394db1166
sreeyuvanash <75154505+sreeyuvanash@users.noreply.github.com>
2021-03-09 17:02:55 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,52 @@
+class Player {
+  constructor(){
+    this.index = null;
+    this.distance = 0;
+    this.name = null;
+
+    this.rank = null
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",(data)=>{
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "players/player" + this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    });
+  }
+
+  getNum(){
+    var playerNum = database.ref('carNum');
+    playerNum.on("value",(data)=>{
+    this.rank = data.val();
+    })
+  }
+
+  static updateRank(carRank){
+   database.ref('/').update({
+    'carNum': carRank
+  }) 
+  }
+
+
+
+  static getPlayerInfo(){
+    var playerInfoRef = database.ref('players');
+    playerInfoRef.on("value",(data)=>{
+      allPlayers = data.val();
+    })
+  }
+}
